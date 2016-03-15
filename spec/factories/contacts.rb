035c10e343ead2c32cfa6b88a71c6e25917cc17b
BlueAccords <myoldemailname@gmail.com>
2016-03-15 12:20:13 -0700

    refactored factories to use Faker data

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -2,10 +2,14 @@
 # Factory file names are not specific and can be whatever.
 FactoryGirl.define do
     factory :contact do
-        firstname "John"
-        lastname "Doe"
+        # a {} block is used because factory girl sees it as a "lazy attribute"
+        # TODO: Do more research on what lazy attribute actually entails
+        # previous version was "static" type
+        firstname { Faker::Name.first_name }
+        lastname  { Faker::Name.last_name }
         # sequence automatically increments n as new contacts are made.
-        sequence(:email) { |n| "johndoe#{n}@example.com"}
+        # sequence(:email) { |n| "johndoe#{n}@example.com"}
+        email     { Faker::Internet.email }
     end
 end
 
